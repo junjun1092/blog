14a4cd587fb2621073b71dd8378e16a1b0383e87
zhaojunjun <zhaojunjun0531@126.com>
2016-07-12 23:09:59 +0800

    beego hello world

---
+++ helloDemo.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/astaxie/beego"
+)
+
+type MainContorller struct {
+	beego.Controller
+}
+
+func (this *MainContorller) Get() {
+	this.Ctx.WriteString("hello world")
+}
+func main() {
+	beego.Router("/", &MainContorller{})
+	beego.Run()
+}
