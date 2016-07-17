4648bc6882208ea977cfbe3cd226bb5e396b2abf
zhaojunjun <zhaojunjun0531@126.com>
2016-07-17 22:29:48 +0800

    建表sql

---
+++ table.sql
@@ -0,0 +1,11 @@
+create table User userId varchar,
+				  name varchar,
+				  age number,
+				  gender varchar,
+				  phone varchar,
+				  email varchar,
+				  comment varchar,
+				  info varchar);				  
+create table Letter letterId varchar,
+                    letterContent varchar,
+					attachement varchar);
