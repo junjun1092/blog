c1dc48dc95d1afbb62b6415f3be6c8572f6105c5
zhaojunjun <zhaojunjun0531@126.com>
2016-08-07 22:37:55 +0800

    开发计划和登录部分内容

--- controllers/login_controller.go
+++ controllers/login_controller.go
@@ -5,5 +5,24 @@ import "beego-blog-master/controllers"
 type LoginContorller struct {
 	controllers.BaseController
 }
+func (this *LoginContorller) login()
+{
+	this.TplNames = "login/login.html"
+}
+func (this *LoginContorller) DoLogin()
+{
+	name := this.GetString("name")
+	if(name == "")
+	{
+		this.Ctx.WriteString("姓名为空")
+		return
+	}
+	password := this.GetString("password")
+	if(password == "")
+	{
+		this.Ctx.WriteString("密码为空")
+		return
+	}
+}
 func main() {
 }
