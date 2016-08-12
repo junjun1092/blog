38b17a6217b16d641c41166f20911531f9b10fe2
zhaojj <zhaojunjun0531@126.com>
2016-08-12 19:27:26 +0800

    完善controller

---
+++ controllers/base_controller.go
@@ -0,0 +1,41 @@
+package controllers
+
+import (
+	"github.com/astaxie/beego"
+	"github.com/g"
+)
+
+type Checker interface {
+	checkLogin()
+}
+type BaseController struct {
+	beego.Controller
+	IsAdmin bool
+}
+
+func (this *BaseController) Prepare(){
+	this.Data["BlogLogo"] = g.BlogLogo
+	this.Data["BlogTitle"] = g.BlogTitle
+	this.Data["BlogResume"] = g.BlogResume
+	this.Data["RootName"] = g.RootName
+	this.Data["RootEmail"] = g.RootEmail
+	this.Data["RootPortrait"] = g.RootPortrait
+	this.AssigonIsAdmin()
+
+}
+
+func (this *BaseController) AssigonIsAdmin(){
+	bb_name := this.Ctx.GetCookie("bb_name")
+	bb_password := this.Ctx.GetCookie("bb_password")
+	if bb_name == "" || bb_password == "" {
+		this.IsAdmin = false
+		return
+	}
+	if bb_name != g.RootName  || bb_password != g.RootPass{
+		this.IsAdmin = false
+	}
+	this.IsAdmin = true
+	this.Data["isAdmin"] = this.IsAdmin
+}
+
+
---
+++ controllers/main_controller.go
@@ -0,0 +1,14 @@
+package controllers
+
+import "github.com/models/catalog"
+
+type MainController struct {
+	BaseController
+}
+func (this *MainController) Get(){
+	this.Data["Catalogs"] = catalog.All()
+	this.Data["PageTitle"] = "首页"
+	this.Layout = "layout/default.html"
+	this.TplName = "index.html"
+}
+
